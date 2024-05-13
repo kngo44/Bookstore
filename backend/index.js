94ff021168e9bd8f6af9971a50bb967671c6a6bf
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-05-12 21:52:57 -0500

    added some lines

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js'
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+//Middleware for handling CORS POLICY
 
 app.get('/', (request, response) => {
     console.log(request);
