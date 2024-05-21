a89db7951204555f3cb5be1a0c383d6fbb5740e6
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-05-21 11:34:30 -0500

    31:39

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInforCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+
+  }, []);
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
