4f2f6687d69ed7cd2f0e9d1a676a1f9c96c2fd71
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-05-27 19:58:28 -0500

    35:21

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
-export const PORT = 500;
+export const PORT = 5555;
 export const mongoDBURL = 'mongodb+srv://root:tjbmy4qkoGeY0TyK@bookstore-mern.lw3ajaw.mongodb.net/?retryWrites=true&w=majority&appName=Bookstore-MERN';
