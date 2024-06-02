9bb257057535bb097cb883f8dfc582eb2f5a3887
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-06-01 20:38:55 -0500

    56:27

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -16,6 +16,18 @@ const BookModal = ({ book, onClose }) => {
           className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
           onClick={onClose}
         />
+        <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
+          {book.publishYear}
+        </h2>
+        <h4 className="my-2 text-gray-500">{book._id}</h4>
+        <div className="flex justify-start items-center gap-x-2">
+          <PiBookOpenTextLight className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.title}</h2>
+        </div>
+        <div className="flex justify-start items-center gap-x-2">
+          <BiUserCircle className="text-red-400 text-2xl" />
+          <h2 className="my-1">{book.author}</h2>
+        </div>
       </div>
     </div>
   );
