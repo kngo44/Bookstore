f761f2caab4f4d8e0f60e4978e175c4529330f60
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-06-03 17:30:36 -0500

    Done

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -12,7 +12,7 @@ const BookModal = ({ book, onClose }) => {
         onClick={(event) => event.stopPropagation()}
         className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
       >
-        <AiOutlineCLose
+        <AiOutlineClose
           className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
           onClick={onClose}
         />
